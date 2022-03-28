5feefbc37131cfe459f84112ac30229457859756
Misha Mansoor <mishatnvr@gmail.com>
2022-03-27 20:59:31 -0400

    added commenting

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,6 +4,14 @@
 //AKA - Anonymous Self-Executing Function
 //Closure - limits scope leak
 
+/** 
+ * Name :   Misha Mansoor and Hunter Lovering 
+ * Student No :     100743787 
+ * Description: this lab is the lab 3 of webd 6201 that deals with single page application 
+ * using AJAX, JQuery and JavaScript
+ * 
+*/
+
 "use strict";
 
 ((core) =>
--- Scripts/namespace.js
+++ Scripts/namespace.js
@@ -1,2 +1,12 @@
+
+/** 
+ * Name :   Misha Mansoor and Hunter Lovering 
+ * Student No :     100743787 
+ * Description: this lab is the lab 3 of webd 6201 that deals with single page application 
+ * using AJAX, JQuery and JavaScript
+ * 
+*/
+
+
 // namespace core
 let core;

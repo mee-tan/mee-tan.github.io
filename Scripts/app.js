28d5e9903b44e2aee04fdda41c90a0e05d7345e6
Misha Mansoor <mishatnvr@gmail.com>
2022-03-27 20:32:00 -0400

    added task list link

--- Scripts/app.js
+++ Scripts/app.js
@@ -382,6 +382,12 @@
         `<a id="logout" class="nav-link" aria-current="page"><i class="fas fa-sign-out-alt"></i> Logout</a>`
         );
 
+        //add task-list page 
+        $(`<li class="nav-item">
+        <a id="task-list" class="nav-link" aria-current="page"><i class="fas fa-users fa-lg"></i> Task List</a>
+      </li>`).insertAfter("#loginListItem");
+
+
         $("#logout").on("click", function()
         {
           // perform logout
